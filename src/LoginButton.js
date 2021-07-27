cf4819103bba0d1a221948e64e00eae60637b732
mtygit <tim.ma0118@gmail.com>
2021-07-27 19:34:15 +0200

    try to fix the redirect in login button

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
 
-  return !isAuthenticated && (<button onClick={() => loginWithRedirect()}>Log In</button>);
+  return !isAuthenticated && (<button onClick={loginWithRedirect}>Log In</button>);
 };
 
 export default LoginButton;
